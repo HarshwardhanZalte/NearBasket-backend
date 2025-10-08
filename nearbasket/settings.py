331f82275c5ab593c3320389665465b4d1cab9ae
[Harshwardhan Zalte] <[harshwardhanzalte9@gmail.com]>
2025-10-09 00:54:12 +0530

    update access token time

--- nearbasket/settings.py
+++ nearbasket/settings.py
@@ -87,7 +87,7 @@ REST_FRAMEWORK = {
 # JWT Configuration
 from datetime import timedelta
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
