c6f2e9bb43fe01b4991ba8c0475487986a64eb45
[Harshwardhan Zalte] <[harshwardhanzalte9@gmail.com]>
2025-11-11 14:31:33 +0530

    fix downtime on server

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('verify-otp/', views.verify_otp, name='verify_otp'),
     path('me/', views.get_profile, name='profile'),
     path('me/update/', views.update_profile, name='update_profile'),
+    
+    path('job/', views.tigger_job, name='tigger_job'),
 ]
