dd8b205ef983c05fbe55d603b3c58b224bf86ee0
Harshwardhan Zalte <109222478+HarshwardhanZalte@users.noreply.github.com>
2025-11-11 14:33:43 +0530

    fixed server downtime (#13)
    * opt service update
    
    * update access token time
    
    * fix downtime on server
    
    ---------
    
    Co-authored-by: [Harshwardhan Zalte] <[harshwardhanzalte9@gmail.com]>

--- users/views.py
+++ users/views.py
@@ -139,3 +139,7 @@ def update_profile(request):
         serializer.save()
         return Response(serializer.data)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def tigger_job(request):
+    return Response({'message': 'Job triggered successfully'}, status=status.HTTP_200_OK)
